fcb5ecb30100b9daaee67aa771c31d451008ced0
67b8 <149882064+67b8@users.noreply.github.com>
2024-05-15 17:26:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", getData);
+
+function getData(){
+    let get=document.getElementById("data");
+    fetch("http://localhost:3000/users")
+    .then(response=>response.json())
+    .then((data)=>{
+        
+        for(let temp of data){
+            
+            var row=document.createElement('tr');
+            let c1 = document.createElement("td")
+            let c2 = document.createElement("td")
+            let c3 = document.createElement("td")
+            c1.innerText=temp.id;
+            c2.innerText=temp.name;
+            c3.innerHTML=`<button onclick=updateData(${temp.id})>EDIT</button><button onclick=deleteData(${temp.id})>DELETE</button>`
+            row.appendChild(c1);
+            row.appendChild(c2);
+            row.appendChild(c3);
+            get.appendChild(row);
+        }
+       
+    });
+}
+
+
+function addData(){
+    var id=document.getElementById("ID").value
+    var name=document.getElementById("NAME").value
+
+    fetch("http://localhost:3000/users",
+        {
+            method:'POST',
+            body:JSON.stringify(
+                {
+                    "id":id,
+                    "name":name
+                }
+            )
+        }
+    )
+    .then(response=>console.log(response))
+    .catch(err=>console.log(err));
+    getData();
+}
+
+
+function deleteData(id){
+    fetch(`http://localhost:3000/users/${id}`,{
+        method:'DELETE'
+    })
+    .then(response=>response.json())
+    .then((data)=>console.log(data));
+}
+
+function updateData(tid){
+    document.getElementById("ID").value=tid;
+    var name=document.getElementById("NAME").value
+     
+    fetch(`http://localhost:3000/users/${tid}`,
+        {
+            method:'PATCH',
+            body:JSON.stringify(
+                {
+                    "name":name
+                }
+            )
+        }
+    )
+    .then(response=>console.log(response))
+    .catch(err=>console.log(err));
+}
